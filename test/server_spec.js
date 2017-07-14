7900b26c38b16e082798f6c26566a650066f3b0f
Mark <suyatdesign@gmail.com>
2017-07-14 16:57:25 -0700

    failing test

--- test/server_spec.js
+++ test/server_spec.js
@@ -6,13 +6,13 @@ var app = require('../server-config.js');
 
 describe('get api', function() {
 
-  it ('GET "/" should ', function(done) {
+  it ('GET "/" should return "Hello!"', function(done) {
 
     request(app)
       .get('/')
       .expect(function(result){
         // console.log('RESULT', result.text)
-        expect(result.text).to.equal('Hello!');
+        expect(result.text).to.equal('He!');
       })
     .end(done);
   });
