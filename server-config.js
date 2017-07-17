df21ffdf5349c91bb89ced2834548c9a258dab08
Semie Rogers <redrogers@gmail.com>
2017-07-17 15:37:57 -0700

    Create status route

--- server-config.js
+++ server-config.js
@@ -1,16 +1,25 @@
 console.log(Date());
 
 var express = require('express');
-
+var handler = require('../lib/request-handler.js');
 var app = express();
 
 app.get('/', function(req, res) {
   res.send('Hello!');
 })
 
-// app.post('/', function(req, res) {
-//   res.send('Hello!');
-// })
+app.get('/status', (req, res) => {
+  handler.getStatus(req.body, (err, status) => {
+    if (err) {
+      res.status(404).send();
+    } else {
+      res.status(201).send(status);
+    }
+  });
+});
 
+app.get('/signup', (req, res) => {
+  handler.signup
+})
 
 module.exports = app;
