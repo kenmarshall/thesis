104f1a6bb46309917ead2f44a5758ab58b9214b4
Semie Rogers <redrogers@gmail.com>
2017-07-17 15:57:53 -0700

    Create put, post and get routing with error handling.

--- server-config.js
+++ server-config.js
@@ -1,12 +1,16 @@
 console.log(Date());
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var handler = require('../lib/request-handler.js');
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
+
+app.get('/', (req, res) => {
   res.send('Hello!');
-})
+});
 
 app.get('/status', (req, res) => {
   handler.getStatus(req.body, (err, status) => {
@@ -18,8 +22,48 @@ app.get('/status', (req, res) => {
   });
 });
 
-app.get('/signup', (req, res) => {
-  handler.signup
-})
+app.post('/signup', (req, res) => {
+  handler.signup(req.body, (err, user) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(user);
+    }
+  });
+});
+
+app.post('/login', (req, res) => {
+  handler.login(req.body, (err, user) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(user);
+    }
+  });
+});
+
+app.post('/logout', (req, res) => {
+  handler.logout(req.body, (err, status) => {
+    if (err) {
+      console.error(err);
+      res.status(400).send();
+    } else {
+      res.status(200).send();
+    }
+  });
+});
+
+app.put('/users', (req, res) => {
+  handler.update(req.body, (err, user) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send();
+    } else {
+      res.status(201).send(user);
+    }
+  });
+});
 
 module.exports = app;
