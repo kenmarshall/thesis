4dfd37d788c65a7700919f9e16bc79441cc79e1d
Kenneth Marshall <kdmarshall89@gmail.com>
2017-07-28 19:05:32 -0500

    fix add favorites

--- server-config.js
+++ server-config.js
@@ -4,16 +4,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var handler = require('./lib/request-handler.js');
 var app = express();
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 app.use(express.static(__dirname + '/public'));
-
 
 app.get('/test', (req, res) => {
   res.send('Hello!');
 });
-
 
 app.get('/status', handler.getStatus);
 
@@ -24,21 +22,12 @@ app.post('/login', handler.login);
 app.put('/edit-profile', handler.editProfile);
 
 app.post('/favorites', handler.addFavorites);
+
 app.delete('/favorites', handler.deleteFavorites);
 
 app.get('/feeds', handler.getFeeds);
 
 //app.post('/logout', handler.logout);
 
-// app.put('/users/:username', (req, res) => {
-//   handler.update(req.body, (err, user) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(404).send();
-//     } else {
-//       res.status(201).send(user);
-//     }
-//   });
-// });
-
 module.exports = app;
+
