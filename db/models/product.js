06c46244ba1b7be00f2e539ed27b72d674ed132a
Kenneth Marshall <kdmarshall89@gmail.com>
2017-07-29 17:07:10 -0500

    fix incorrect results for favorites and add tests

--- db/models/product.js
+++ db/models/product.js
@@ -3,11 +3,14 @@ var Schema = mongoose.Schema;
 var db = require ('../config');
 
 var ProductSchema = new Schema({
+  upc: String,
   title: String,
   image: String,
   info: String,
+  nutrients: [],
   ingredients: [String],
-  likes: Number
+  likes: Number,
+  reviews:[]
 });
 
 var Product = mongoose.model('Product', ProductSchema);
